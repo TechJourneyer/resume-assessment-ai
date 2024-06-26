45aab3ba1b0729d45a85b7a1253946a11ed5f0d2
Swapnil Mahadik <swap.1710@gmail.com>
2024-06-26 22:10:39 +0530

    dbmigration

---
+++ resume_app.sql
@@ -0,0 +1,16 @@
+-- Adminer 4.8.1 MySQL 5.7.44 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+INSERT INTO `job_requirements` (`id`, `role_name`, `qualification`, `experience_years`, `skills`, `description`, `created_by`, `created_at`, `updated_at`) VALUES
+(1,	'PHP Developer',	'BE',	2,	'PHP,JAVASCRIPT',	'FULL STACK WEB DEVELOPER',	1,	'2023-05-20 09:44:09',	'2023-05-23 12:52:55'),
+(2,	'Seniour PHP Developer',	'BE',	4,	'PHP , MYSQL , JAVASCRIPT ',	'We are seeking an experienced and talented Sr. PHP Full Stack Developer to join our dynamic development team. As a Sr. PHP Full Stack Developer, you will play a key role in designing, developing, and maintaining high-quality web applications and solutions. You will be responsible for both front-end and back-end development, ensuring optimal performance, security, and user experience.',	1,	'2023-05-29 10:04:59',	'2023-05-29 10:04:59');
+
+
+
+
+
+-- 2024-06-26 16:40:06
